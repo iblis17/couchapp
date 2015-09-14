c1f752c0b6c4d97d70757850585be4d868eb9335
Iblis Lin <e196819@hotmail.com>
2015-09-14 11:41:11 +0800

    [#176] Test case for Config.update

--- tests/test_config.py
+++ tests/test_config.py
@@ -37,7 +37,7 @@ def test_config_init_local(rcpath, getcwd, local_conf):
     Test case for Config.__init__() in a CouchApp
     '''
     config = Config()
-    
+
     local_conf.assert_called_with('/mockapp')
     assert config.local_conf == {'mock': True}, config.local_conf
 
@@ -149,3 +149,13 @@ class TestConfig():
         '''
         self.config.load_local(None)
         assert not load.called
+
+    @patch('couchapp.config.Config.load_local', return_value={'mock': True})
+    def test_update(self, load_local):
+        '''
+        Test case for Config.update()
+        '''
+        self.config.update('/mock')
+
+        assert self.config.conf.get('mock') == True
+        load_local.assert_called_with('/mock')
